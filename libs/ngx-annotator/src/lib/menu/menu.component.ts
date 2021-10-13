b363170687741e572795e48b1acdd3a14659cbe5
Val Neekman <val@neekware.com>
2021-10-13 15:23:16 -0400

    LineWithPicker

--- libs/ngx-annotator/src/lib/menu/menu.component.ts
+++ libs/ngx-annotator/src/lib/menu/menu.component.ts
@@ -30,7 +30,8 @@ export class MenuComponent implements OnDestroy {
   cursorIconState = 'back';
   isFullscreen = false;
 
-  lineWithValues: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  lineWithValues: number[] = [1, 2, 3, 5, 7, 9];
+
   lineColorValues: string[] = [
     '#ffffff',
     '#ff0000',
