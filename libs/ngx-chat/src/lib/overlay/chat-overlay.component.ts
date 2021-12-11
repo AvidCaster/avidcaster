c18ad245f24a85705a6a320b4e7cb602f468af8d
Val Neekman <val@neekware.com>
2021-12-11 18:47:19 -0500

    remove duplicate provider

--- libs/ngx-chat/src/lib/overlay/chat-overlay.component.ts
+++ libs/ngx-chat/src/lib/overlay/chat-overlay.component.ts
@@ -17,7 +17,6 @@ import { ChatService } from '../chat.service';
   templateUrl: './chat-overlay.component.html',
   styleUrls: ['./chat-overlay.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [ChatService],
 })
 export class ChatOverlayComponent implements OnInit {
   private onStorageOb$: Observable<Event>;
