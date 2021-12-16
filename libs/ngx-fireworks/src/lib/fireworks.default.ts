844b2f115bb56a3f8d389b7d77bb2fac3540b5de
Val Neekman <val@neekware.com>
2021-12-15 19:35:55 -0500

    fireworks fast

--- libs/ngx-fireworks/src/lib/fireworks.default.ts
+++ libs/ngx-fireworks/src/lib/fireworks.default.ts
@@ -11,8 +11,8 @@ import { cloneDeep as ldDeepClone } from 'lodash-es';
 import { FireworksOptions } from './fireworks.model';
 
 export const DefaultFireworksOptions: FireworksOptions = {
-  rocketSpawnInterval: 150,
-  maxRockets: 3,
+  rocketSpawnInterval: 170,
+  maxRockets: 5,
   numParticles: 100,
   explosionMinHeight: 0.2,
   explosionMaxHeight: 0.9,
